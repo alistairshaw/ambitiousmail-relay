22bfabc0d71a033f7c9a6d0cad56798963a52b3d
Alistair Shaw <alistairshaw@gmail.com>
2015-03-18 19:38:08 +0000

    QF

--- app/Http/Controllers/QueueConsumers/QueueConsumerController.php
+++ app/Http/Controllers/QueueConsumers/QueueConsumerController.php
@@ -7,12 +7,6 @@ class QueueConsumerController extends Controller {
 
 	public function __construct(Queue $queue)
 	{
-		// whenever a queue consumer is called, we need to add a new consumer to replace the
-		//  one we're using
-		/*$queueName = Request::input('queue');
-		$queueUrl = Request::input('url');
-		$queue->start($queueName);
-		$queue->consume($queueUrl);*/
 
 	}
 
