bb69792b44d6a2486de97919a4523a542aef3d38
Alistair Shaw <alistairshaw@gmail.com>
2015-03-20 01:16:05 +0000

    Email logging

--- app/Http/Controllers/QueueConsumers/EmailsController.php
+++ app/Http/Controllers/QueueConsumers/EmailsController.php
@@ -4,6 +4,7 @@ use App\AmbitiousMailSender\Base\Services\HttpRequest\HttpRequest;
 use App\AmbitiousMailSender\Base\Services\MailTransport\MailTransport;
 use App\AmbitiousMailSender\CampaignEmails\CampaignEmailRepository;
 use App\AmbitiousMailSender\Campaigns\CampaignRepository;
+use Log;
 use Request;
 
 class EmailsController extends QueueConsumerController {
@@ -34,6 +35,9 @@ class EmailsController extends QueueConsumerController {
 			$campaignId   = $msg->campaignId;
 			$emailsToSend = $msg->emailsToSend;
 		}
+
+		Log::info('Email Sending Queue Consumer - Campaign: ' . $campaignId);
+		exit();
 
 		if ($campaign = $campaignRepository->find($campaignId))
 		{
